d9ace449ef24ffa6fd16cf143004322e11dc1ab9
Anant Kumawat <anantkumawat22@gmail.com>
2021-02-06 18:53:09 +0530

    My first Commit ReactWebsite

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+let greet;
+let greetcss ={
+    color:'green',
+}
+let date = new Date();
+date = date.getHours();
+if(date >= 1 && date < 12){
+    greet = "Good Morning.";
+    greetcss.color = 'green'; 
+}
+else if(date >= 12 && date < 16){
+    greet = "Good Afternoon.";
+    greetcss.color = 'orange';
+}
+else if(date >= 16 && date < 20){
+    greet = "Good Evening.";
+    greetcss.color = 'blue';
+}
+else{
+    greet = "Good Night.";
+    greetcss.color = 'purple';
+}
+
+function App(){
+
+    return(
+        <div>
+            <h1>Hello Sir, <span style = {greetcss}>{greet}</span></h1>
+        </div>
+    );
+}
+
+export default App;
